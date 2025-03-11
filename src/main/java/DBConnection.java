6f597a5307405263ec4fd9a3f04714e22f8480e2
Alexander Burgos <axb19c@acu.edu>
2025-03-11 15:11:58 -0500

    Changes chatbot file to dbconnection

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class DBConnection {
+
+    private Connection connection = null;
+
+    public void connect() {
+        // Connect to the database
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:chatbotdb");
+        } catch (SQLException e) {
+            System.err.println("Failed to connect to the database: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    // Close the database connection
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Failed to close the database connection: " + e.getMessage());
+        }
+    }
+}
