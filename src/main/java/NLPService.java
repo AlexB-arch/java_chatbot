53dfe21a4e1ac6c73f37cc1ba838ecb2b74632e3
Alexander Burgos <axb19c@acu.edu>
2025-03-12 12:05:58 -0500

    Adds openNLP

---
+++ src/main/java/NLPService.java
@@ -0,0 +1,34 @@
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NLPService {
+    private SentenceDetectorME sentenceDetector;
+    private TokenizerME tokenizer;
+    private DocumentCategorizerME categorizer;
+
+    public NLPService() {
+        try {
+            // initialze basic models
+            initializeModels();
+        } catch (IOException e) {
+            System.err.println("Error initializing NLP models: " + e.getMessage());
+        }
+    }
+
+    private void initializeModels() throws IOException {
+        // Load sentence detector model
+
+        // Load tokenizer model
+    }
+}
