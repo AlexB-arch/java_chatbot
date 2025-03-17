cbbbcba0944bd080a3fdb83405b68c91de09b523
Alexander Burgos <axb19c@acu.edu>
2025-03-17 10:27:09 -0500

    Adds more tests

--- src/test/java/DatabaseServiceTests.java
+++ src/test/java/DatabaseServiceTests.java
@@ -48,4 +48,18 @@ public class DatabaseServiceTests {
         results.forEach(System.out::println);
         Assert.assertTrue(results.size() == 1);
     }
+
+    @Test
+    public void searchByKeywords() {
+        List<Map<String, Object>> results = databaseService.executeQuery("SELECT * FROM major WHERE title LIKE ?", "%Accounting%");
+        results.forEach(System.out::println);
+        Assert.assertTrue(results.size() == 1);
+    }
+
+    @Test
+    public void dynamicSelectGrades() {
+        List<Map<String, Object>> results = databaseService.executeQuery("SELECT id, CASE WHEN id = ? THEN ? ELSE 'Unknown' END AS RequiredGrade FROM course WHERE id = ?;", "ACCT210", "C", "ACCT210");
+        results.forEach(System.out::println);
+        Assert.assertTrue(results.size() == 1);
+    }
 }
